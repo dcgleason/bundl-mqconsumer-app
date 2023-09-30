f07b5d478eeef6ce9f3a4171d670a7a03591a8ab
dcgleason <danny.c.gleason@gmail.com>
2023-09-30 11:21:04 -0400

    update env

--- src/messageConsumer.js
+++ src/messageConsumer.js
@@ -1,8 +1,9 @@
+require('dotenv').config(); // Make sure you have this line to load environment variables
 const amqp = require('amqplib/callback_api');
-const sendEmail = require('./utils/sendEmail'); // Import your sendEmail function
+const sendEmail = require('./utils/sendEmail');
 
-// Use your specific AmazonMQ AMQP endpoint
-const amazonMqUrl = 'amqps://b-5b5885c9-d33b-41aa-94fc-faad831a859f.mq.us-east-1.amazonaws.com:5671';
+// Use environment variable for AmazonMQ URL
+const amazonMqUrl = process.env.AMAZON_MQ_URL;
 
 amqp.connect(amazonMqUrl, (error, connection) => {
   if (error) throw error;
